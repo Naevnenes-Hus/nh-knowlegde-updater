377c74dba0bee90fa4cd6a9877a688e910541509
KlingerSaw <108529290+KlingerSaw@users.noreply.github.com>
2025-10-30 13:21:14 +0100

    Remove Python implementation

--- afgorelsesportalen/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities for syncing decisions from Afgørelsesportalen."""
-
-from .sync import handle_prompt, update_decision_knowledge
-
-__all__ = ["handle_prompt", "update_decision_knowledge"]
